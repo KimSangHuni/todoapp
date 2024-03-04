7df09f3ebd70ea28257a6584d24cc5f06501748a
KimSangHuni <dev.sanghoun@gmail.com>
2024-03-04 22:27:05 +0900

    feat: styled-component 기본 container & Typography 추가

---
+++ todo/src/components/container/container.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background: gray;
+    min-height: 100vh;
+`
+
+export const Wrapper = styled.div`
+    max-width: 800px;
+    margin: 0 auto;
+`
+
+export const BoxFlex = styled.div`
+    display: flex;
+`
+
+export const BoxGrid = styled.div`
+    display: grid;
+`
+
+export default Container;
